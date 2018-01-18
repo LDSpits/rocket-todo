73c81dec9d01833d556cb7047c2415b48a1af99c
Lucas Spits <spits.lucas@gmail.com>
2018-01-18 14:35:05 +0100

    Add static file serving

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+use rocket::response::NamedFile;
+use rocket::response::status::NotFound;
+
+use std::path::PathBuf;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = std::env::current_dir().unwrap().join("assets").join(file);
+    NamedFile::open(&path).map_err(|_| NotFound(format!("Bad path: {}", path.display())))
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, files])
+        .launch();
 }
